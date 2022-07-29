e612f13bf8ab3d7a156a98adfe5852dd5eef96a7
Volnetiks <volnetiks@gmail.com>
2022-07-29 22:47:41 +0200

    Finished appBar UI

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -8,8 +8,54 @@ class NotePage extends StatefulWidget {
 }
 
 class _NotePageState extends State<NotePage> {
+  bool pinned = false;
+  bool notificationsEnabled = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back, color: Colors.black)),
+      actions: [
+        IconButton(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          visualDensity: const VisualDensity(horizontal: -4.0, vertical: -4.0),
+          icon: Icon(pinned ? Icons.push_pin : Icons.push_pin_outlined,
+              color: Colors.black),
+          onPressed: () {
+            setState(() {
+              pinned = !pinned;
+            });
+          },
+        ),
+        IconButton(
+          visualDensity: const VisualDensity(horizontal: -4.0, vertical: -4.0),
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+          icon: Icon(
+              notificationsEnabled
+                  ? Icons.notifications
+                  : Icons.notifications_outlined,
+              color: Colors.black),
+          onPressed: () {
+            setState(() {
+              notificationsEnabled = !notificationsEnabled;
+            });
+          },
+        ),
+        IconButton(
+          visualDensity: const VisualDensity(horizontal: -4.0, vertical: -4.0),
+          icon: const Icon(Icons.access_time, color: Colors.black),
+          onPressed: () {},
+        )
+      ],
+    ));
   }
 }
