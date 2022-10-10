f2679e0959414bbaa2b90d65ea14d753b35d495f
Volnetiks <volnetiks@gmail.com>
2022-10-10 13:00:17 +0200

    Added fluttertoast

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -24,38 +24,43 @@ class _NoteWidgetState extends State<NoteWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-            color: colors[widget.note.colorID],
-            borderRadius: const BorderRadius.all(Radius.circular(25))),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              widget.note.title == ""
-                  ? Container(height: 0)
-                  : Text(widget.note.title,
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold)),
-              widget.note.title == ""
-                  ? Container(height: 0)
-                  : const SizedBox(height: 5),
-              widget.note.content == ""
-                  ? Container(height: 0)
-                  : Text(widget.note.content,
-                      style: const TextStyle(fontSize: 15)),
-              const SizedBox(height: 15),
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black, width: 0.5),
-                      borderRadius: const BorderRadius.all(Radius.circular(5))),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(widget.note.dueDate.formatNotesDate()),
-                  ))
-            ],
-          ),
-        ));
+    return GestureDetector(
+      onDoubleTap: () => {},
+      child: Container(
+          decoration: BoxDecoration(
+              color: colors[widget.note.colorID],
+              borderRadius: const BorderRadius.all(Radius.circular(25))),
+          child: Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                widget.note.title == ""
+                    ? Container(height: 0)
+                    : Text(widget.note.title,
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                widget.note.title == ""
+                    ? Container(height: 0)
+                    : const SizedBox(height: 5),
+                widget.note.content == ""
+                    ? Container(height: 0)
+                    : Text(widget.note.content,
+                        style: const TextStyle(fontSize: 15)),
+                const SizedBox(height: 15),
+                Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black, width: 0.5),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(5))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(widget.note.dueDate.formatNotesDate()),
+                    ))
+              ],
+            ),
+          )),
+    );
   }
 }
